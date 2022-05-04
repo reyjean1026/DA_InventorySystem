44792217abd0b9a757c3b4b5738b587beed0e83e
unknown <Rey Jean Comaling>
2022-05-04 16:19:56 +0800

    Update version 4

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -376,4 +376,18 @@
         </div>
       </div>
     </div>
+    <footer class="footer pt-3">
+        <div class="container-fluid">
+        <div class="row align-items-center justify-content-lg-between">
+            <div class="col-lg-6 mb-lg-0 mb-4">
+            <div class="copyright text-center text-sm text-white text-lg-start">
+                Copyright © <script>
+                document.write(new Date().getFullYear())
+                </script>,
+                <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">DA Caraga - ICTU</a>
+            </div>
+            </div>
+        </div>
+        </div>
+    </footer>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -144,7 +144,7 @@
             </nav>
             <!-- End Navbar -->
             @yield('content')
-            <footer class="footer pt-3">
+           <!-- <footer class="footer pt-3">
                 <div class="container-fluid">
                 <div class="row align-items-center justify-content-lg-between">
                     <div class="col-lg-6 mb-lg-0 mb-4">
@@ -157,7 +157,7 @@
                     </div>
                 </div>
                 </div>
-            </footer>
+            </footer>-->
         </main>
     <!--
         <div class="fixed-plugin">
